97d597e4dc1d0b220ba5aba10572cdbd163f7778
Misaki Kinjo <misaki0827bitodemi@gmail.com>
2022-03-12 09:50:07 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 金城美咲 on 2022/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
